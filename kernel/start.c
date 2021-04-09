5a8c88c2664b05ab2ed327b758aa048dfbb997f4
Tom Pillot <tom.pillot@pm.me>
2021-04-09 16:43:09 +0200

    initial commit

---
+++ kernel/start.c
@@ -0,0 +1,30 @@
+#include <n7OS/cpu.h>
+#include <inttypes.h>
+#include <n7OS/processor_structs.h>
+
+
+void fibonacci() {
+  int i, nb1, nb2, res, n;
+  n=5;
+  for (i=0;i<n;i++) {
+    if (i <= 1) {
+      res = i;
+    } else {
+      res = nb1 + nb2;
+      nb1 = nb2;
+      nb2 = res;
+    }
+  }
+}
+
+
+void kernel_start(void)
+{
+    fibonacci();
+    
+    // on ne doit jamais sortir de kernel_start
+    while (1) {
+        // cette fonction arrete le processeur
+        hlt();
+    }
+}
---
+++ kernel/syscall_defs.c
@@ -0,0 +1,9 @@
+#include <n7OS/syscall_defs.h>
+
+fn_ptr syscall_table[NB_SYSCALL];
+
+void add_syscall(int num, fn_ptr function) {
+  if (num < NB_SYSCALL) {
+    syscall_table[num]= function;
+  }
+}
